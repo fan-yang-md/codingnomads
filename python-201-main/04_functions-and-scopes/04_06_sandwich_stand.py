5cc5c76bb3ecdb9e33fae519e5b0755d53a78d4c
Fan Yang <fancito@gmail.com>
2023-05-16 16:28:11 -0700

    04_06 n".join(list_name)

--- python-201-main/04_functions-and-scopes/04_06_sandwich_stand.py
+++ python-201-main/04_functions-and-scopes/04_06_sandwich_stand.py
@@ -3,3 +3,12 @@
 # - takes an arbitrary amount of toppings
 # - returns a string representing a sandwich with the bread on top
 #   and bottom, and the toppings in between.
+
+def make_sandwich():
+    bread = input('What kind of bread? ')
+    toppings = input('Now choose your toppings: ')
+    topping_list = toppings.split()
+    sandwich = bread + '\n' + '\n'.join(topping_list) + bread
+    return sandwich
+
+print(f'Here is your sandwich: \n{make_sandwich()}')
