66d9b856353a85b7b401ebd66aa3dccaafe1561c
Fan Yang <fancito@gmail.com>
2023-05-26 14:30:01 -0700

    more python 101 labs

--- python-101-main/11_conditionals-and-loops/11_09_primes.py
+++ python-101-main/11_conditionals-and-loops/11_09_primes.py
@@ -1,1 +1,13 @@
 # Print out every prime number between 1 and 1000.
+
+list = []
+
+for num in range(1,1001):
+    count = 0
+    for divisor in range(2,num):
+        if num%divisor == 0:
+            count += 1
+    if count == 0:
+        list.append(num)
+
+print(list)
